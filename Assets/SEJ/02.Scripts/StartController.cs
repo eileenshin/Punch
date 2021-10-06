695b8d9e7117e4caaa3fe42cffd71ca0c1e57441
eunjishin <87408515+eileenshin@users.noreply.github.com>
2021-10-06 12:08:33 +0900

    /.

--- Assets/SEJ/02.Scripts/StartController.cs
+++ Assets/SEJ/02.Scripts/StartController.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartController : MonoBehaviour
 {
     public OVRInput.Controller hand;
-    LineRenderer line;
-    public Transform trRIght;
+    public LineRenderer line;
+    public Transform trRight;
     public Transform trLeft;
 
     IEnumerator Vibration()
@@ -25,9 +26,42 @@ public class StartController : MonoBehaviour
     
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, hand))
         {
+            Ray ray = new Ray(trRight.position, trRight.forward);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                line.gameObject.SetActive(true);
+                line.SetPosition(0, trRight.position);
+                line.SetPosition(1, hit.point);
+                //만약에 부딪힌 놈이 Button이라면 
+                Button btn = hit.transform.GetComponent<Button>();
+                //Button에 OnClick에 등록된 함수를 실행
+                if (btn != null)
+                {
+                    btn.onClick.Invoke();
+                }
+            }
+            else
+            {
+                line.gameObject.SetActive(false);
+            }
             StartCoroutine(Vibration());
         }
+        if(OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, hand))
+        {
+            if(line.gameObject.activeSelf)
+            {
+                line.gameObject.SetActive(false);
+
+            }
+        }
+
+          
+
+
+
+
     }
 }
