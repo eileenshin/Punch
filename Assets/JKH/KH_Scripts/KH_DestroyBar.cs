81675a0a5506bde9a704578d60e361b6ca4c0d7c
jkh5039 <borm3403@naver.com>
2021-10-07 17:11:09 +0900

    a

--- Assets/JKH/KH_Scripts/KH_DestroyBar.cs
+++ Assets/JKH/KH_Scripts/KH_DestroyBar.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class KH_DestroyBar : MonoBehaviour
 {
-    public GameObject Standard;
+   
     public OVRInput.Controller hand;
     //public float dist1;
     // Start is called before the first frame update
@@ -30,33 +30,7 @@ public class KH_DestroyBar : MonoBehaviour
     {
         if (other.gameObject.tag == "Node")
         {
-            float dist1 = Mathf.Abs(Standard.transform.position.y - other.transform.position.y);
-            print(KH_ScoreManager.instance.Combo);
-
-
-            if (dist1 < .7f)
-            {
-                print("Perfect");
-                print("debug"+transform.name);
-                KH_ScoreManager.instance.AddScore(100);
-
-            }
-            else if (dist1 < 1.2f)
-            {
-                print("Cool");
-                KH_ScoreManager.instance.AddScore(70);
-
-            }
-
-            else
-            {
-                print("Miss");
-                //KH_ScoreManager.instance.CurrScore = 0;
-                KH_ScoreManager.instance.currHP -= 10;
-                KH_ScoreManager.instance.Combo = 0;
-                KH_ScoreManager.instance.MissCnt += 1;
-            }
-            Destroy(other.gameObject);
+            
 
             StartCoroutine(Vibration());
         }
