99778460c0ea3db5512031cdc6f30b16200c1da3
jkh5039 <borm3403@naver.com>
2021-10-13 17:04:40 +0900

    asd

--- Assets/JKH/KH_Scripts/KH_NewMusic.cs
+++ Assets/JKH/KH_Scripts/KH_NewMusic.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class KH_NewMusic : MonoBehaviour
 {
+    public bool isNewMusic1 = false;
+    public static KH_NewMusic instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        isNewMusic1 = true;
     }
 
     // Update is called once per frame
